237e336017ab6d868d3bc4efb181a0834282d655
uiux@okdollar <uiux@okdollar.com>
2025-07-11 13:53:08 +0630

    fix

--- src/payments/payments.controller.ts
+++ src/payments/payments.controller.ts
@@ -1,24 +1,24 @@
 import {
   Controller,
   Get,
-  Post,
-  Body,
-  Patch,
+  // Post,
+  // Body,
+  // Patch,
   Param,
   Delete,
 } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
-import { CreatePaymentDto } from './dto/create-payment.dto';
-import { UpdatePaymentDto } from './dto/update-payment.dto';
+// import { CreatePaymentDto } from './dto/create-payment.dto';
+// import { UpdatePaymentDto } from './dto/update-payment.dto';
 
 @Controller('payments')
 export class PaymentsController {
   constructor(private readonly paymentsService: PaymentsService) {}
 
-  @Post()
-  create(@Body() createPaymentDto: CreatePaymentDto) {
-    return this.paymentsService.create(createPaymentDto);
-  }
+  // @Post()
+  // create(@Body() createPaymentDto: CreatePaymentDto) {
+  //   return this.paymentsService.create(createPaymentDto);
+  // }
 
   @Get()
   findAll() {
@@ -30,10 +30,10 @@ export class PaymentsController {
     return this.paymentsService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePaymentDto: UpdatePaymentDto) {
-    return this.paymentsService.update(+id, updatePaymentDto);
-  }
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updatePaymentDto: UpdatePaymentDto) {
+  //   return this.paymentsService.update(+id, updatePaymentDto);
+  // }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@nestjs/common';
-import { CreatePaymentDto } from './dto/create-payment.dto';
-import { UpdatePaymentDto } from './dto/update-payment.dto';
+// import { CreatePaymentDto } from './dto/create-payment.dto';
+// import { UpdatePaymentDto } from './dto/update-payment.dto';
 
 @Injectable()
 export class PaymentsService {
-  create(createPaymentDto: CreatePaymentDto) {
-    return 'This action adds a new payment';
-  }
+  // create(createPaymentDto: CreatePaymentDto) {
+  //   return 'This action adds a new payment';
+  // }
 
   findAll() {
     return `This action returns all payments`;
@@ -16,9 +16,9 @@ export class PaymentsService {
     return `This action returns a #${id} payment`;
   }
 
-  update(id: number, updatePaymentDto: UpdatePaymentDto) {
-    return `This action updates a #${id} payment`;
-  }
+  // update(id: number, updatePaymentDto: UpdatePaymentDto) {
+  //   return `This action updates a #${id} payment`;
+  // }
 
   remove(id: number) {
     return `This action removes a #${id} payment`;
